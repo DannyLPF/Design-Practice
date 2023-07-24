fe9ba2da89cee57a125ac77605f828894ab911e3
DannyLPF <daniel.simkiss@gmail.com>
2023-07-24 15:55:27 +0100

    add map instead of foreach to js

--- scripts/load-collections.js
+++ scripts/load-collections.js
@@ -1,10 +1,8 @@
-let htmlInjection = '';
+const htmlInjectionArray = collections.map((value) => `
+    <div class="content-collections">
+        <img class="collections-image" src="images/collections/${value.image}.png">
+    </div>
+`);
 
-collections.forEach((value) => { htmlInjection += `
-        <div class="content-collections">
-            <img class="collections-image" src="images/collections/${value.image}.png">
-        </div>
-    `;
-});
-
+const htmlInjection = htmlInjectionArray.join('');
 document.querySelector('.js-content').innerHTML = htmlInjection;
